a6341e80bc19fd2c66e652f9b2e0d84e1d14b980
Deepanshu Pathak <dhruvpathak365@gmail.com>
2024-03-16 20:02:33 +0530

    Modify navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,34 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./index.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <nav class="bg-white dark:bg-gray-900 sticky w-full h-24 z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="" class="h-8" alt="" />
-            <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">
+      <nav className="bg-gray-900 sticky top-0 z-20">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              src=""
+              className="h-8"
+              alt=""
+            />
+            <span className="self-center text-4xl font-semibold whitespace-nowrap text-white">
               OTA'x
             </span>
           </a>
-          <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex rtl:space-x-reverse">
             <button
+              onClick={toggleMenu}
               type="button"
-              class="text-white bg-cyan-500 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-3xl px-8 py-3 text-center dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            >
-              Login
-            </button>
-            <button
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              class="inline-flex items-center p-2 w-12 h-12 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-12 h-12 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
               aria-controls="navbar-sticky"
-              aria-expanded="false"
+              aria-expanded={isMenuOpen ? "true" : "false"}
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="w-5 h-5"
+                className={`w-5 h-5 ${isMenuOpen ? "hidden" : ""}`}
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -36,32 +40,48 @@ function Navbar() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+              <svg
+                className={`w-5 h-5 ${isMenuOpen ? "" : "hidden"}`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
             </button>
           </div>
           <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMenuOpen ? "" : "hidden"
+            }`}
             id="navbar-sticky"
           >
-            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col gap-7 p-4 md:p-0 mt-4 font-medium md:flex-row md:mt-0 ">
               <li>
                 <Link
-                  to="/"
-                  class="block text-3xl py-2 px-3 text-white bg-cyan-700 rounded md:bg-transparent md:text-cyan-700 md:p-0 md:dark:text-cyan-500"
-                  aria-current="page"
+                  to="/home"
+                  className="block text-xl py-2 px-3 text-gray-100 hover:text-cyan-700 rounded md:bg-transparent md:p-0 "
+                  area-current="page"
                 >
                   Home
                 </Link>
               </li>
               <li>
                 <Link
-                  to="/about"
-                  class="block text-3xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 md:dark:hover:text-cyan-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  t0="/about"
+                  className="block text-xl py-2 px-3 text-gray-100 rounded hover:text-cyan-700 md:p-0 "
                 >
                   About
                 </Link>
@@ -69,9 +89,9 @@ function Navbar() {
               <li>
                 <Link
                   to="/contact"
-                  class="block text-3xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 md:dark:hover:text-cyan-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block text-xl py-2 px-3 text-gray-100 rounded hover:text-cyan-700 md:p-0"
                 >
-                  Help/Contact
+                  Help
                 </Link>
               </li>
             </ul>
